a824ad02fbcb6d456feccac2b42efe996ee54bad
Jake O'Rielly <jake.orielly@gmail.com>
2021-02-12 01:16:59 +0000

    Created planter box component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <PlanterBox />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PlanterBox from './components/PlanterBox.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PlanterBox
   }
 }
 </script>
---
+++ src/components/PlanterBox.vue
@@ -0,0 +1,25 @@
+<template>
+  <table>
+    <tr v-for="row in rows" :key="row">
+      <td v-for="col in cols" :key="`${row}-${col}`">
+        Plant Goes Here
+      </td>
+    </tr>
+  </table>
+</template>
+
+<script>
+export default {
+  name: 'PlanterBox',
+  data() {
+    return {
+      rows: 3,
+      cols: 3
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
