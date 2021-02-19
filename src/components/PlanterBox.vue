eb97dd2081fcd0665d2850da610176432996f370
Jake O'Rielly <jake.orielly@gmail.com>
2021-02-19 00:06:25 +0000

    Highlight selected cell

--- src/components/PlanterBox.vue
+++ src/components/PlanterBox.vue
@@ -3,6 +3,7 @@
     <tr v-for="row in $store.state.rows" :key="row">
       <td 
         v-for="col in $store.state.cols" :key="`${row}-${col}`"
+        :class="{'selected' : isSelected(row - 1, col - 1)}"
         @click="clickCell(row - 1, col - 1)"  
       >
         <img 
@@ -23,6 +24,9 @@ export default {
     clickCell(x, y) {
       this.$store.commit("setSelectedCell", [x, y]);
     },
+    isSelected(x, y) {
+      return this.$store.state.selectedCell == x * 3 + y;
+    },
     getPlantImage(x, y) {
       return this.$store.getters.getImg([x, y]);
     }
@@ -33,5 +37,16 @@ export default {
 <style scoped>
 table {
   display: inline-block;
+}
+
+td {
+  border: 2px solid black;
+  border-radius: 0.25rem;
+  width: 2rem;
+  height: 2rem;
+}
+
+.selected {
+  border: 2px solid gold;
 }
 </style>
